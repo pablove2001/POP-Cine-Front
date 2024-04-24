561a046b755c02281e9ae4f123771005b15bf9cb
Pablo Vergara <vergarapablo2001@gmail.com>
2024-04-23 19:15:53 -0600

    no cache

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -2,7 +2,8 @@ import { Movie } from "@/types/Movie";
 
 export const fetchMovies = async () => {
   const res = await fetch(
-    "http://movies.us-east-1.elasticbeanstalk.com/movies"
+    "http://movies.us-east-1.elasticbeanstalk.com/movies",
+    { cache: "no-cache" }
   );
   const data: Movie[] = await res.json();
   console.log(data);
